9d26b19b383b126ef37383f98cf88f1608f72055
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-12 14:51:27 -0700

    Add Chair() class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+class Chair {
+private:
+    int m_legs{};
+    vector<double> m_prices{};
+public:
+    Chair() : m_legs{0}, m_prices{} {};
+};
 
 int main() {
 
