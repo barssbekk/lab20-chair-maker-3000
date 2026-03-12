ac1dcd717213f2273beef50fefc36cc32d2107f3
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-12 15:01:23 -0700

    Adding getter/setter

--- main.cpp
+++ main.cpp
@@ -9,7 +9,13 @@ private:
     vector<double> m_prices{};
 public:
     Chair() : m_legs{0}, m_prices{} {};
+    // Setter
+    void setLegs(int userLegs) { m_legs = userLegs; }
+
+    // Getter
+    int getLegs() {}
 };
+
 
 int main() {
 
