5d5fd8eab5c77eb5a81196a4c0723e88c4796aa4
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-12 15:31:28 -0700

    Rewrite the code

--- main.cpp
+++ main.cpp
@@ -1,25 +1,74 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
 
 using namespace std;
+const int SIZE = 3;
 
 class Chair {
 private:
-    int m_legs{};
-    vector<double> m_prices{}; // FIXME: double array
+    int legs;
+    double * prices;
 public:
-    Chair() : m_legs{0}, m_prices{} {};
-    // Setter
-    void setLegs(const int userLegs) { m_legs = userLegs; }
-    // FIXME: double array
-    void setPrices(const vector<double>& userPrices) { m_prices = userPrices; }
+    // constructors
+    Chair() {
+        prices = new double[SIZE];
+        legs = 0;
+        for (int i = 0; i < SIZE; i++)
+            prices[i] = 0;
+    }
 
-    // Getter
-    int getLegs() { return m_legs; }
-    vector<double> getPrices() { return m_prices; }
+    Chair(int l) {
+        prices = new double[SIZE];
+        legs = l;
+        for (int i = 0; i < SIZE; i++)
+            prices[i] = 0;
+    }
+    // setters and getters
+    void setLegs(int l) { legs = l; }
+    int getLegs() { return legs; }
+    void setPrices(double p1, double p2, double p3) {
+        prices[0] = p1; prices[1] = p2; prices[2] = p3;
+    }
+    double getAveragePrices() {
+        double sum = 0;
+        for (int i = 0; i < SIZE; i++)
+            sum += prices[i];
+        return sum / SIZE;
+    }
+    void print() {
+        cout << "CHAIR DATA - legs: " << legs << endl;
+        cout << "Price history: " ;
+        for (int i = 0; i < SIZE; i++)
+            cout << prices[i] << " ";
+        cout << endl << "Historical avg price: " << getAveragePrices();
+        cout << endl << endl;
+    }
 };
 
-
 int main() {
+    cout << fixed << setprecision(2);
+    //creating pointer to first chair object
+    Chair *chairPtr = new Chair;
+    chairPtr->setLegs(4);
+    chairPtr->setPrices(121.21, 232.32, 414.14);
+    chairPtr->print();
 
+    //creating dynamic chair object with constructor
+    Chair *livingChair = new Chair(3);
+    livingChair->setPrices(525.25, 434.34, 252.52);
+    livingChair->print();
+    delete livingChair;
+    livingChair = nullptr;
+    //creating dynamic array of chair objects
+    Chair *collection = new Chair[SIZE];
+    collection[0].setLegs(4);
+    collection[0].setPrices(441.41, 552.52, 663.63);
+    collection[1].setLegs(4);
+    collection[1].setPrices(484.84, 959.59, 868.68);
+    collection[2].setLegs(4);
+    collection[2].setPrices(626.26, 515.15, 757.57);
+    for (int i = 0; i < SIZE; i++)
+        collection[i].print();
+    return 0;
 }
+
