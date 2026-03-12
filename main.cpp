782258a63451f27762c028184e27919aed6c1012
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-12 15:20:35 -0700

    Comments/Reminders

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 class Chair {
 private:
     int m_legs{};
-    vector<double> m_prices{};
+    vector<double> m_prices{}; // FIXME: double array
 public:
     Chair() : m_legs{0}, m_prices{} {};
     // Setter
     void setLegs(const int userLegs) { m_legs = userLegs; }
+    // FIXME: double array
     void setPrices(const vector<double>& userPrices) { m_prices = userPrices; }
 
     // Getter
